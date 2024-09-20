311436254534b5f48dcc160d2544815ec937c053
Melih Günaydın <84282430+mgunaydin4@users.noreply.github.com>
2024-09-20 03:35:47 +0300

    Update Pusula_Melih_Gunaydin.py

--- Pusula_Melih_Gunaydin.py
+++ Pusula_Melih_Gunaydin.py
@@ -156,6 +156,40 @@ def handle_missing_values(df):
 
 handle_missing_values(df)
 
+def outlier_thresholds(dataframe, col_name, q1=0.05, q3=0.95):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+
+# Analiz fonksiyonu
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+def replace_with_thresholds(dataframe, variable, q1=0.05, q3=0.95):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable, q1=0.05, q3=0.95)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+
+# Aykırı Değer Analizi ve Baskılama İşlemi
+
+num_cols = [i for i in df.columns if df.dtypes[i] in ["int64", "float64"]]
+
+for col in num_cols:
+    print(col, check_outlier(df, col))
+    if check_outlier(df, col):
+        replace_with_thresholds(df, col)
+
+
+
 
 
 ############################################## Verilerin Görselleştirilmesi ##############################################
